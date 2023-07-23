965f5bf0201826534aa293d5d1e5787abb86213b
Xkylz <fauzansaputra741@gmail.com>
2023-07-24 02:35:14 +0800

    Main Feature

---
+++ src/main/java/org/example/ToDoListApp.java
@@ -0,0 +1,43 @@
+package org.example;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ToDoListApp {
+
+    // deklarasi variabel global
+    static String fileName;
+    static ArrayList<String> todoLists;
+    static boolean isEditing = false;
+    static Scanner input;
+
+    public static void main(String[] args) {
+
+    }
+
+    static void clearScreen(){
+    }
+
+    static void showMenu() {
+    }
+
+    static void backToMenu() {
+    }
+
+    static void readTodoList() {
+    }
+
+    static void showTodoList() {
+    }
+
+    static void addTodoList() {
+    }
+
+    static void editTodoList() {
+    }
+
+    static void deleteTodoList() {
+    }
+
+}
